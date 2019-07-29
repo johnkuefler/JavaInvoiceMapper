b49ee3bc4efd4919376e99a0389b516b6cb57e87
John Kuefler <john@dev-squared.com>
2019-07-29 16:36:01 -0500

    agent management started

--- src/main/java/invoicemapper/dal/AgentDataService.java
+++ src/main/java/invoicemapper/dal/AgentDataService.java
@@ -6,7 +6,8 @@
 package invoicemapper.dal;
 
 import invoicemapper.lib.Agent;
-import invoicemapper.lib.Sale;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -15,32 +16,80 @@ import java.util.ArrayList;
  * @author johnk
  */
 public class AgentDataService extends DataService {
-    
+
     public AgentDataService() {
         super();
     }
-    
-    public ArrayList<Agent> GetById(int agentId) throws SQLException {
-         // todo: implement
+
+    public ArrayList<Agent> GetByClientName(String clientName) throws SQLException {
+        ArrayList<Agent> output = new ArrayList<Agent>();
+
+        PreparedStatement statement = super.getConnection().prepareStatement("SELECT Id, ClientName, FirstName, LastName  FROM IM_Agent WHERE ClientName = ?");
+        statement.setString(1, clientName);
+        ResultSet resultSet = statement.executeQuery();
+
+        while (resultSet.next()) {
+            Agent agent = new Agent();
+
+            agent.setId(resultSet.getString(0));
+            agent.setClientName(resultSet.getString(1));
+            agent.setFirstName(resultSet.getString(2));
+            agent.setLastName(resultSet.getString(3));
+
+            output.add(agent);
+        }
+
+        return output;
+    }
+
+    public Agent GetById(String agentId) throws SQLException {
+
+        PreparedStatement statement = super.getConnection().prepareStatement("SELECT Id, ClientName, FirstName, LastName  FROM IM_Agent WHERE Id = ?");
+        statement.setString(1, agentId);
+        ResultSet resultSet = statement.executeQuery();
+
+        while (resultSet.next()) {
+            Agent agent = new Agent();
+
+            agent.setId(resultSet.getString(0));
+            agent.setClientName(resultSet.getString(1));
+            agent.setFirstName(resultSet.getString(2));
+            agent.setLastName(resultSet.getString(3));
+
+            return agent;
+        }
+
         return null;
     }
-    
-     public ArrayList<Agent> GetAll() throws SQLException {
-         // todo: implement
-        return null;
+
+    public void Create(Agent newAgent) throws SQLException {
+        PreparedStatement statement = super.getConnection().prepareStatement("INSERT INTO IM_Agent(Id, ClientName, FirstName, LastName) " 
+        + "VALUES (?,?,?,?)");
+        
+        statement.setString(1, newAgent.getId());
+        statement.setString(2, newAgent.getClientName());
+        statement.setString(3, newAgent.getFirstName());
+        statement.setString(4, newAgent.getLastName());
+       
+        statement.executeUpdate();
     }
-    
-    public Agent Create(Agent newSale) throws SQLException {
-      // todo: implement
-        return null;
+
+    public void Update(Agent agentToUpdate) throws SQLException {
+          PreparedStatement statement = super.getConnection().prepareStatement("UPDATE IM_Agent " 
+        + "SET FirstName = ?, LastName = ? WHERE Id = ?");
+        
+        statement.setString(1, agentToUpdate.getFirstName());
+        statement.setString(2, agentToUpdate.getLastName());
+        statement.setString(3, agentToUpdate.getId());
+       
+        statement.executeUpdate();
     }
-    
-    public Agent Update(Sale sqleToUpdate) throws SQLException {
-      // todo: implement
-        return null;
-    }
-     
-    public void Delete(int agentId) throws SQLException {
-      // todo: implement
+
+    public void Delete(String agentId) throws SQLException {
+           PreparedStatement statement = super.getConnection().prepareStatement("DELETE From IM_Agent WHERE Id = ?");
+        
+        statement.setString(1, agentId);
+        
+        statement.executeUpdate();
     }
 }
